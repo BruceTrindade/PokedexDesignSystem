0cc2b17b2022824fa76954626c70c9e45fd5a08e
brucetrindade <brucetrindade2000@gmail.com>
2022-10-18 08:10:33 -0300

    release card

--- dsmpokedex/src/main/java/com/example/dsmpokedex/CardView.kt
+++ dsmpokedex/src/main/java/com/example/dsmpokedex/CardView.kt
@@ -1,14 +1,23 @@
 package com.example.dsmpokedex
 
 import android.content.Context
+import android.util.AttributeSet
 import android.widget.ImageView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import com.google.android.material.card.MaterialCardView
 
 class PokeCardView : MaterialCardView {
-    constructor(
-        context: Context
-    ) : super(context) {
+
+    constructor(context: Context?) : super(context) {
+        initialization()
+    }
+
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs) {
+        initialization()
+    }
+
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
         initialization()
     }
 
@@ -19,6 +28,15 @@ class PokeCardView : MaterialCardView {
         strokeColor = ContextCompat.getColor(context, R.color.white)
         strokeWidth = 2
 
+        imageView.layoutParams.height = 30
+        imageView.layoutParams.width = 30
         imageView.setImageDrawable(context.getDrawable(pokeballImage))
+
+        val params = imageView.layoutParams as ConstraintLayout.LayoutParams
+        params.leftToRight = this.id
+        params.topToTop = ConstraintLayout.LayoutParams.UNSET
+        params.bottomToBottom = this.id
+        this.requestLayout()
+
     }
 }
