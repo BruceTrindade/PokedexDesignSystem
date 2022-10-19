9310687b4a53522da89dab4fcb419f1e6b11fa45
brucetrindade <brucetrindade2000@gmail.com>
2022-10-19 13:35:48 -0300

    PokeCardView

---
+++ dsmpokedex/src/main/java/com/example/dsmpokedex/PokeCardView.kt
@@ -0,0 +1,17 @@
+package com.example.dsmpokedex
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.example.dsmpokedex.databinding.CardViewPokeBinding
+
+class CardViewPoke @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet,
+    defStyleAttr: Int = 0
+) : ConstraintLayout(context, attrs, defStyleAttr) {
+
+    private val binding = CardViewPokeBinding
+        .inflate(LayoutInflater.from(context), this, true)
+}
