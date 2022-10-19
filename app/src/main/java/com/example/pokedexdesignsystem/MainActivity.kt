157a6e48d28d1c9f4f2f6e16711d2a5ff3d11b77
brucetrindade <brucetrindade2000@gmail.com>
2022-10-19 18:17:32 -0300

    Card view update

--- app/src/main/java/com/example/pokedexdesignsystem/MainActivity.kt
+++ app/src/main/java/com/example/pokedexdesignsystem/MainActivity.kt
@@ -33,7 +33,6 @@ class MainActivity : ComponentActivity() {
                         Greeting("Android")
                         PokeBallBackground(text = "Pikachu")
                     }
-
                 }
             }
         }
--- dsmpokedex/src/main/java/com/example/dsmpokedex/PokeCardView.kt
+++ dsmpokedex/src/main/java/com/example/dsmpokedex/PokeCardView.kt
@@ -3,19 +3,16 @@ package com.example.dsmpokedex
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
-import android.view.ViewGroup
+import android.widget.FrameLayout
 import com.example.dsmpokedex.databinding.CardViewPokeBinding
 
 class CardViewPoke @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet,
     defStyleAttr: Int = 0
-) : ViewGroup(context, attrs, defStyleAttr) {
+) : FrameLayout(context, attrs, defStyleAttr) {
 
     private val binding = CardViewPokeBinding
         .inflate(LayoutInflater.from(context), this, true)
 
-    override fun onLayout(p0: Boolean, p1: Int, p2: Int, p3: Int, p4: Int) {
-        TODO("Not yet implemented")
-    }
 }
