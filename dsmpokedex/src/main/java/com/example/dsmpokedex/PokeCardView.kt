da01b9fae7a511f908f4c681477b3d5825b3dd50
brucetrindade <brucetrindade2000@gmail.com>
2022-10-19 17:03:02 -0300

    Card view update

--- dsmpokedex/src/main/java/com/example/dsmpokedex/PokeCardView.kt
+++ dsmpokedex/src/main/java/com/example/dsmpokedex/PokeCardView.kt
@@ -3,15 +3,19 @@ package com.example.dsmpokedex
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
-import android.widget.FrameLayout
+import android.view.ViewGroup
 import com.example.dsmpokedex.databinding.CardViewPokeBinding
 
 class CardViewPoke @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet,
     defStyleAttr: Int = 0
-) : FrameLayout(context, attrs, defStyleAttr) {
+) : ViewGroup(context, attrs, defStyleAttr) {
 
     private val binding = CardViewPokeBinding
         .inflate(LayoutInflater.from(context), this, true)
+
+    override fun onLayout(p0: Boolean, p1: Int, p2: Int, p3: Int, p4: Int) {
+        TODO("Not yet implemented")
+    }
 }
