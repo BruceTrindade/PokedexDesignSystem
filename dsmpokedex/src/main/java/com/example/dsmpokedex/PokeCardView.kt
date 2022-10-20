7420be531a55cbf5277e3bd4c77b0a45851b110e
brucetrindade <brucetrindade2000@gmail.com>
2022-10-20 07:38:36 -0300

    poke card

--- dsmpokedex/src/main/java/com/example/dsmpokedex/PokeCardView.kt
+++ dsmpokedex/src/main/java/com/example/dsmpokedex/PokeCardView.kt
@@ -1,6 +1,7 @@
 package com.example.dsmpokedex
 
 import android.content.Context
+import android.graphics.drawable.Drawable
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.FrameLayout
@@ -14,5 +15,25 @@ class CardViewPoke @JvmOverloads constructor(
 
     private val binding = CardViewPokeBinding
         .inflate(LayoutInflater.from(context), this, true)
+
+
+
+    fun setPokeName(text: String) {
+        binding.pokemonName.text = text
+    }
+
+    fun setPokeType(type: String) {
+        binding.pokemonType.text = type
+    }
+
+    fun setPokeStype(sType: String) {
+        binding.pokemonSecondType.text = sType
+    }
+
+    fun setPokeImage(image: Drawable) {
+        binding.pokemonImg.setImageDrawable(image)
+    }
+
+
 
 }
