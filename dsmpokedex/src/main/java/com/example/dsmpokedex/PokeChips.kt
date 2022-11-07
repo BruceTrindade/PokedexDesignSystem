dc72b3e7a196576c6c10c17ba2d7db1e7d041bb7
brucetrindade <brucetrindade2000@gmail.com>
2022-11-07 07:10:36 -0300

    fix constraints chips

--- dsmpokedex/src/main/java/com/example/dsmpokedex/PokeChips.kt
+++ dsmpokedex/src/main/java/com/example/dsmpokedex/PokeChips.kt
@@ -2,18 +2,18 @@ package com.example.dsmpokedex
 
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.*
 import androidx.compose.material3.AssistChip
+import androidx.compose.material3.AssistChipDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PokeChips(text: String, primaryColor: Int = R.color.white) {
-
     val color = Color(primaryColor)
 
     AssistChip(
